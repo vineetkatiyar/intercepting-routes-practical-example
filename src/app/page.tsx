c521576659398eea61dfdae805a216e46d6a42a6
Vineet Katiyar <vineetkatiyar98@gmail.com>
2025-01-28 21:32:26 +0530

    creating some changes in page.tsx in root dirctory

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,17 @@
+import Link from "next/link";
 export default function Home() {
   return (
-    <>
-      <main>
-        <h1>This is home page</h1>
-      </main>
-    </>
+    <main className=" flex justify-center container h-full w-full">
+      <div className="">
+        <h1 className="text-center font-bold text-3xl mt-10">
+          This is home page
+        </h1>
+        <button className="text-center m-3 bg-green-400 p-2 rounded-md text-white">
+          <Link href="/photo-feed" className="text-center">
+            click here to check the intercepting route photo gallery
+          </Link>
+        </button>
+      </div>
+    </main>
   );
 }
